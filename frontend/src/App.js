a0eabc2bf32b40160613fd560288e07711f27713
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-10-28 22:38:04 +0200

    added intial frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+const App = () => {
+	return (
+		<div className='categories-container'>
+			<div className='category-container'>
+				{/* <img /> */}
+				<div className='category-body-container'>
+					<h2>Guitars</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img /> */}
+				<div className='category-body-container'>
+					<h2>Basses</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img /> */}
+				<div className='category-body-container'>
+					<h2>Drums</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img /> */}
+				<div className='category-body-container'>
+					<h2>Keys</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img /> */}
+				<div className='category-body-container'>
+					<h2>Accessories</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
