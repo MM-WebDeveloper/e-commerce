1c390d4ad62f7d6a5cbf303a427f2db576ce4308
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-11-20 19:47:33 +0100

    added css design to the front page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,29 +5,41 @@ const App = () => {
 		{
 			id: 1,
 			title: 'Guitars',
+			imageUrl:
+				'https://cdn.mos.cms.futurecdn.net/4RTTiaPRfdQh7x9X3AZXzf-1200-80.jpg',
 		},
 		{
 			id: 2,
 			title: 'Basses',
+			imageUrl: 'https://cdn.mos.cms.futurecdn.net/aH8MAWKFDvKc4y82LGaNHm.jpg',
 		},
 		{
 			id: 3,
 			title: 'Keys',
+			imageUrl:
+				'https://hammondorgans.com.au/wp-content/uploads/2017/07/Home-1200x500.jpg',
 		},
 		{
 			id: 4,
 			title: 'Drums',
+			imageUrl:
+				'https://cdn.shopify.com/s/files/1/1422/9358/products/GRETSCH-USA-CUSTOM-SSB-70-S-4-PIECE-DRUM-KIT-ROSEWOOD-LACQUER-DRUMAZON_14_1000x.jpg?v=1591830862',
 		},
 		{
 			id: 5,
 			title: 'Vocals',
+			imageUrl:
+				'https://martinmitchellsmicrophones.files.wordpress.com/2020/12/akg-d130-3-1.jpg',
 		},
 	];
 	return (
 		<div className='categories-container'>
-			{categories.map(({ title }) => (
-				<div className='category-container'>
-					<div className='background-image' />
+			{categories.map(({ title, id, imageUrl }) => (
+				<div key={id} className='category-container'>
+					<div
+						className='background-image'
+						style={{ backgroundImage: `url(${imageUrl})` }}
+					/>
 					<div className='category-body-container'>
 						<h2>{title}</h2>
 						<p>Shop Now</p>
