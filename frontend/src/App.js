33d886edb9180f0d2aa549686f98554d97b89402
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-11-22 22:11:30 +0100

    added routing and landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,25 @@
-import './categories.styles.scss';
-import Directory from './components/directory/directory';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import HomeComponent from './routes/home/home';
+
+const Nav = () => {
+	return (
+		<div>
+			<div>
+				<h1>navigation bar</h1>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
 
 const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'Guitars',
-			imageUrl:
-				'https://cdn.mos.cms.futurecdn.net/4RTTiaPRfdQh7x9X3AZXzf-1200-80.jpg',
-		},
-		{
-			id: 2,
-			title: 'Basses',
-			imageUrl: 'https://cdn.mos.cms.futurecdn.net/aH8MAWKFDvKc4y82LGaNHm.jpg',
-		},
-		{
-			id: 3,
-			title: 'Keys',
-			imageUrl:
-				'https://hammondorgans.com.au/wp-content/uploads/2017/07/Home-1200x500.jpg',
-		},
-		{
-			id: 4,
-			title: 'Drums',
-			imageUrl:
-				'https://cdn.shopify.com/s/files/1/1422/9358/products/GRETSCH-USA-CUSTOM-SSB-70-S-4-PIECE-DRUM-KIT-ROSEWOOD-LACQUER-DRUMAZON_14_1000x.jpg?v=1591830862',
-		},
-		{
-			id: 5,
-			title: 'Vocals',
-			imageUrl:
-				'https://martinmitchellsmicrophones.files.wordpress.com/2020/12/akg-d130-3-1.jpg',
-		},
-	];
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<Nav />}>
+				<Route index element={<HomeComponent />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
