e3e7660b332cb3dd594161f8cbd5ea1a9147a892
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-11-28 23:57:55 +0100

    added firebase sign in with google popup functionality

--- frontend/src/routes/sign-in/sign-in.component.js
+++ frontend/src/routes/sign-in/sign-in.component.js
@@ -1,9 +1,16 @@
 import './sign-in.component.css';
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+	const logGoogleUser = async () => {
+		const response = await signInWithGooglePopup();
+		console.log(response);
+	};
+
 	return (
 		<div>
 			<p>SignIn</p>
+			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
 		</div>
 	);
 };
--- frontend/src/utils/firebase/firebase.utils.js
+++ frontend/src/utils/firebase/firebase.utils.js
@@ -1,1 +1,27 @@
 import { initializeApp } from 'firebase/app';
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDb3-CeYmP3oYQVI7ZUGPVLj3OaG8xOnuk',
+	authDomain: 'rock-n--roll-instruments.firebaseapp.com',
+	projectId: 'rock-n--roll-instruments',
+	storageBucket: 'rock-n--roll-instruments.appspot.com',
+	messagingSenderId: '838769067253',
+	appId: '1:838769067253:web:6e2f0227c41f04398a71bc',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+	prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
