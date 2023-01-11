829fbd6569bb068d6f5ea680d52a17fae3954d2d
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-01-11 16:29:24 +0100

    added sign in with google redirect

--- frontend/src/routes/sign-in/sign-in.component.js
+++ frontend/src/routes/sign-in/sign-in.component.js
@@ -1,13 +1,29 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import './sign-in.component.css';
 import {
+	auth,
 	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+	useEffect(() => {
+		const getAuthData = async () => {
+			const response = await getRedirectResult(auth);
+
+			if (response) {
+				const userDocRef = await createUserDocumentFromAuth(response.user);
+				console.log(userDocRef);
+			}
+		};
+		getAuthData();
+	}, []);
+
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
-
 		const userDocRef = await createUserDocumentFromAuth(user);
 	};
 
@@ -15,6 +31,9 @@ const SignIn = () => {
 		<div>
 			<p>SignIn</p>
 			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			<button onClick={signInWithGoogleRedirect}>
+				Sign in with Google Redirect
+			</button>
 		</div>
 	);
 };
